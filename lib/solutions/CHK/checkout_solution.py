db9ca5bd9c7f09bfdfa00992216aa01adf9cd67e
sbx_user1051 <sbx_user1051@169.254.44.113>
2022-06-12 16:22:36 +0000

    Sun Jun 12 16:22:36 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ def checkout(skus):
     
     cnt = np.zeros((26,1))
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    cost = [50, 30, 20, 15, 40, 10, 20, 10, 35]
+    cost = [50, 30, 20, 15, 40, 10, 20, 10, 35, 60, 80, 90, 15, 40, 10, 50, 30, 50, 30, 20, 40, 50, 20, 90, 10, 50]
     
     #verify for repeated elements
     for cha in skus:
@@ -53,8 +53,6 @@ def checkout(skus):
         #Offer K
         elif i == 10:
             tcost = tcost + offer_type_b(cnt[i],2,150,80)
-        #Offer H
-        elif i == 7:
         #Offer P
         elif i == 15:
             tcost = tcost + offer_type_b(cnt[i],5,200,50)
@@ -63,6 +61,7 @@ def checkout(skus):
             tcost = tcost + offer_type_b(cnt[i],3,80,30)
         #Offer U
         elif i == 20:
+            tcost = tcost + offer_type_f(cnt[i],3+1,cost[i])
         #Offer V
         elif i == 21:
             tcost = tcost + offer_type_a(cnt[i],3,130,2,90,cost[i])
@@ -82,7 +81,7 @@ def offer_type_a(cnt, bo, bop, so, sop, sp):
 def offer_type_b(cnt,o,op,sp):
     nbr_off = cnt//o 
     single = cnt - nbr_off*o 
-    cost_type_b = nbr_off_b*op + single*sp
+    cost_type_b = nbr_off*op + single*sp
     return cost_type_b
 
 def offer_type_f(cnt,o,sp):
@@ -94,6 +93,7 @@ def offer_type_f(cnt,o,sp):
 out = checkout("EE")
 print(out)
 #
+
 
 
 
