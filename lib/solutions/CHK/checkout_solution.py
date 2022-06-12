b902082676f4ba8c67cc89931206ff2a27362fb1
sbx_user1051 <sbx_user1051@169.254.36.137>
2022-06-12 15:27:24 +0000

    Sun Jun 12 15:27:24 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,7 @@ def checkout(skus):
     b = 0
     c = 0
     d = 0 
+    e = 0
     
     #verify for repeated elements
     for cha in skus:
@@ -19,66 +20,31 @@ def checkout(skus):
             c = c + 1
         elif cha == 'D':
             d = d + 1
+        elif cha == 'E':
+            e = e + 1
         else:
-            return -1
+            return -1 
+        
+    nbr_offers_e = e//2
+    b = b - nbr_offers_e
+    cost_e = e * 40
     
-#    if ( (cnt_a > 1) or (cnt_b > 1) or (cnt_c > 1) or (cnt_d > 1)):
-#        return -1
-    
-#    #extract number of products
-#    a_idx = skus.find('A')
-#    
-#    if a_idx == -1:
-#        a = 0
-#    elif a_idx == 0:
-#        a = 1
-#    else:
-#        a = int(skus[0:a_idx])
-#        
-#    b_idx = skus.find('B')
-#    
-#    if b_idx == -1:
-#        b = 0
-#    elif b_idx == 0:
-#        b = 1
-#    else:
-#        b = int(skus[a_idx+1:b_idx])
-#        
-#    c_idx = skus.find('C')
-#        
-#    if c_idx == -1:
-#        c = 0
-#    elif c_idx == 0:
-#        c = 1
-#    else:
-#        c = int(skus[b_idx+1:c_idx])   
-#    
-#    d_idx = skus.find('D')
-#    
-#    if d_idx == -1:
-#        d = 0
-#    elif d_idx == 0:
-#        d = 1
-#    else:
-#        d = int(skus[c_idx+1:d_idx]) 
-#    
-#    print('A = '+str(a)+' B = '+str(b)+' C = '+str(c)+' D = '+str(d))
-    
-    nbr_offers_a = (a//3) 
-    single_a = a - nbr_offers_a*3 
+    nbr_offers_a_5 = a//5
+    nbr_offers_a_3 = a//3
+    single_a_3 = a - nbr_offers_a_3*3 
     cost_a = nbr_offers_a*130 + single_a*50
     
-    nbr_offers_b = (b//2) 
+    nbr_offers_b = b//2 
     single_b = b - nbr_offers_b*2 
     cost_b = nbr_offers_b*45 + single_b*30
     
     cost_c = c * 20
     cost_d = d * 15
     
-    total_cost = cost_a + cost_b + cost_c + cost_d
-    
-#    print(total_cost)
-    
+    total_cost = cost_a + cost_b + cost_c + cost_d + cost_e
+        
     return total_cost
     
-#out = checkout("a")
+out = checkout("a")
+print(out)
+
