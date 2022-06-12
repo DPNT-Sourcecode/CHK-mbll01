8277d9fb9118e62d87d3ac4bf14c0621cc869de6
sbx_user1051 <sbx_user1051@169.254.3.209>
2022-06-12 14:38:04 +0000

    Sun Jun 12 14:38:04 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,20 +5,17 @@
 def checkout(skus):
     #extract number of products
     a_idx = skus.find('A')
-    a_str = skus[0:a_idx]
-    a = int(a_str)
-    
     b_idx = skus.find('B')
-    b_str = skus[a_idx+1:b_idx]
-    b = int(b_str)
-    
     c_idx = skus.find('C')
-    c_str = skus[b_idx+1:c_idx]
-    c = int(c_str)
-    
     d_idx = skus.find('D')
-    d_str = skus[c_idx+1:d_idx]
-    d = int(d_str)
+    
+    if ( (a_idx == -1) or (b_idx == -1) or (c_idx == -1) or (d_idx == -1)):
+        return -1
+    
+    a = int(skus[0:a_idx])
+    b = int(skus[a_idx+1:b_idx])
+    c = int(skus[b_idx+1:c_idx])
+    d = int(skus[c_idx+1:d_idx])
     
     print('A = '+str(a)+' B = '+str(b)+' C = '+str(c)+' D = '+str(d))
     
@@ -26,6 +23,7 @@ def checkout(skus):
     
     
 checkout("1A20B5C0D")
+
 
 
 
