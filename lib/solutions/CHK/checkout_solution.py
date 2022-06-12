8ce3c08eaf7789e588e403c848c77672f37bc2a1
sbx_user1051 <sbx_user1051@169.254.3.209>
2022-06-12 14:32:04 +0000

    Sun Jun 12 14:32:04 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,5 +4,11 @@
 # skus = unicode string
 def checkout(skus):
     a_idx = skus.find('A')
-    a_str = skus[0:a_idx-1]
+    a_str = skus[0:a_idx]
+    a = int(a_str)
+    print(a)
+    
+    
+checkout("1A20B5C0D")
+
 
