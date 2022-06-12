f72bde56d17a1d82abd019b5b5594b48c8fa7e02
sbx_user1051 <sbx_user1051@169.254.44.113>
2022-06-12 15:40:10 +0000

    Sun Jun 12 15:40:10 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,6 +9,7 @@ def checkout(skus):
     c = 0
     d = 0 
     e = 0
+    f = 0
     
     #verify for repeated elements
     for cha in skus:
@@ -22,12 +23,14 @@ def checkout(skus):
             d = d + 1
         elif cha == 'E':
             e = e + 1
+        elif cha == 'F':
+            f = f + 1
         else:
             return -1 
         
-    nbr_offers_e = e//2
+    nbr_off_e = e//2
     if b>0:
-        b = b - nbr_offers_e
+        b = b - nbr_off_e
     cost_e = e * 40
     
     nbr_off_a5 = a//5
@@ -36,14 +39,18 @@ def checkout(skus):
     single_a = after_a5 - nbr_off_a3*3 
     cost_a = nbr_off_a5*200 + nbr_off_a3*130 + single_a*50
     
-    nbr_offers_b = b//2 
-    single_b = b - nbr_offers_b*2 
-    cost_b = nbr_offers_b*45 + single_b*30
+    nbr_off_b = b//2 
+    single_b = b - nbr_off_b*2 
+    cost_b = nbr_off_b*45 + single_b*30
     
     cost_c = c * 20
     cost_d = d * 15
     
-    total_cost = cost_a + cost_b + cost_c + cost_d + cost_e
+    nbr_off_f = f//3
+    f = f - nbr_off_f
+    cost_f = f * 10
+    
+    total_cost = cost_a + cost_b + cost_c + cost_d + cost_e + cost_f
         
     return total_cost
     
